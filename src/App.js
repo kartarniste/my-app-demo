d7a9a70ed2bc498e00e51ef6eea8a09061aaf830
kartar.niste <kartar.niste@yagnaiq.com>
2019-12-31 10:19:22 +0530

    React project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card, ListGroup, ListGroupItem, Spinner } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MainApp extends Component{
+  constructor(){
+    super();
+    this.state={
+      employees :null,
+      showLoading : false
+    }
+  }
+
+  getDataFromServer = ()=>{
+    this.setState({showLoading : true},()=>{
+      fetch('http://dummy.restapiexample.com/api/v1/employees')
+      .then(response => response.json())
+      .then((data) => {
+      this.setState({employees : data, showLoading : false});
+      })
+      .then((myJson) => {
+      console.log(myJson);
+      });
+    })
+  }
+
+  renderEmployeeSection=(employees)=>{
+    if(employees){
+      return (
+          <Card style={{ width: '18rem'}}>
+            <Card.Body>
+              <Card.Title>Employee Info</Card.Title>
+               <ListGroup className="list-group-flush">
+                  {
+                    employees.map((obj) =>
+                      <ListGroupItem>{obj.employee_name}</ListGroupItem>
+                    )
+                  }
+                </ListGroup>
+              </Card.Body>
+            </Card>
+      )
+    }
+    return null;
+  }
+
+  showMask=(showLoading)=>{
+    if(showLoading){
+      return (
+        <Spinner animation="border" role="status">
+           <span className="sr-only">Loading...</span>
+        </Spinner>
+      )
+    }
+    return null;
+    
+  }
+
+  render(){
+     let { employees, showLoading } = this.state;
+      return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Button onClick={this.getDataFromServer}>Get Employee Info</Button>
+            {this.showMask(showLoading)}
+            {this.renderEmployeeSection(employees)}
+        </div>
+      );
+  }
 }
 
-export default App;
+export default MainApp;
